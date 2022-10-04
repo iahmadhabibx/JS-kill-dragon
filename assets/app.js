f40545c2c56da9d874109e585c10d28d485de73b
Ahmad Habib <iahmadhabibx@gmail.com>
2022-10-04 15:31:54 +0500

    Initialize setup

---
+++ assets/app.js
@@ -0,0 +1,43 @@
+window.addEventListener("load", () => {
+    // canvas setup
+    const canvas = document.getElementById("canvas1");
+    const ctx = canvas.getContext("2d")
+    canvas.width = 500;
+    canvas.height = 500;
+
+    class InputHandler {
+
+    }
+
+    class Projecttile {
+
+    }
+
+    class Particle {
+
+    }
+
+    class Player {
+
+    }
+
+    class Enemy {
+
+    }
+
+    class Layer {
+
+    }
+
+    class Background {
+
+    }
+
+    class UI {
+        
+    }
+
+    class Game {
+        
+    }
+})
