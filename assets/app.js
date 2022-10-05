8ed2504b5d2b6b3dacad819bd577d10c28166648
Ahmad Habib <iahmadhabibx@gmail.com>
2022-10-05 14:53:51 +0500

    Assets added, Work on classes start

--- assets/app.js
+++ assets/app.js
@@ -6,6 +6,18 @@ window.addEventListener("load", () => {
     canvas.height = 500;
 
     class InputHandler {
+        constructor(game) {
+            this.game = game;
+            window.addEventListener('keydown', (e) => {
+                if (((e.key === 'ArrowUp') || (e.key === 'ArrowDown')) && this.game.keys.indexOf(e.key) === -1)
+                    this.game.keys.push(e.key);
+            });
+
+            window.addEventListener('keyup', (e) => {
+                if (this.game.keys.indexOf(e.key) > -1)
+                    this.game.keys.splice(this.game.keys.indexOf(e.key), 1)
+            })
+        }
 
     }
 
@@ -18,6 +30,20 @@ window.addEventListener("load", () => {
     }
 
     class Player {
+        constructor(game) {
+            this.game = game;
+            this.width = 120;
+            this.height = 190;
+            this.x = 20;
+            this.y = 100;
+            this.speedY = 0;
+        }
+        update() {
+            this.y += this.speedY;
+        }
+        draw(context) {
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
 
     }
 
@@ -34,10 +60,34 @@ window.addEventListener("load", () => {
     }
 
     class UI {
-        
+
     }
 
     class Game {
-        
+        constructor(width, height) {
+            this.width = width;
+            this.height = height;
+            this.player = new Player(this);
+            this.input = new InputHandler(this);
+            this.keys = [];
+        }
+
+        update() {
+            this.player.update();
+        }
+        draw(context) {
+            this.player.draw(context);
+        }
     }
+
+    const game = new Game(canvas.width, canvas.height);
+
+    // Animate Loop
+    const animate = () => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        game.update();
+        game.draw(ctx);
+        requestAnimationFrame(animate)
+    }
+    animate()
 })
