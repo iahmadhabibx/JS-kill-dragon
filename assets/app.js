517f6a6a1929c9474951bae062fd6240381395ad
Ahmad Habib <iahmadhabibx@gmail.com>
2022-10-07 16:28:56 +0500

    Add player

--- assets/app.js
+++ assets/app.js
@@ -13,7 +13,8 @@ window.addEventListener("load", () => {
                     this.game.keys.push(e.key);
                 else if (e.key === ' ') {
                     this.game.player.shootTop();
-                }
+                } else if (e.key === 'd')
+                    this.game.debugMode = !this.game.debugMode
             });
 
             window.addEventListener('keyup', (e) => {
@@ -55,23 +56,35 @@ window.addEventListener("load", () => {
             this.height = 190;
             this.x = 20;
             this.y = 100;
+            this.frameX = 0;
+            this.frameY = 0;
+            this.maxFrame = 37;
             this.speedY = 0;
-            this.maxSpeed = 2;
+            this.maxSpeed = 3;
             this.projectiles = [];
+            this.image = document.getElementById("player")
         }
         update() {
             if (this.game.keys.includes('ArrowUp')) this.speedY = -this.maxSpeed;
             else if (this.game.keys.includes('ArrowDown')) this.speedY = this.maxSpeed;
             else this.speedY = 0;
             this.y += this.speedY;
+
+            // Handle projectiles
             this.projectiles.forEach(projectile => {
                 projectile.update();
             });
             this.projectiles = this.projectiles.filter(projectile => !projectile.markedForDeletion);
+
+            // Sprite animation
+            if (this.frameX < this.maxFrame)
+                this.frameX++;
+            else
+                this.frameX;
         }
         draw(context) {
-            context.fillStyle = 'green';
-            context.fillRect(this.x, this.y, this.width, this.height);
+            if (this.game.debugMode) context.strokeRect(this.x, this.y, this.width, this.height);
+            context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
             this.projectiles.forEach(projectile => {
                 projectile.draw(context);
             });
@@ -233,6 +246,7 @@ window.addEventListener("load", () => {
             this.gameTime = 0;
             this.timeLimit = 5000;
             this.speed = 1;
+            this.debugMode = true;
         }
 
         update(deltaTime) {
